36cac870f5a1f90e435ab57eee630991f555d4c7
NandoMiron <147566902+NandoMiron@users.noreply.github.com>
2023-10-15 16:16:00 -0300

    Add files via upload

---
+++ Produto.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula42
+{
+    internal class Produto
+    {
+        public string Nome;
+        public double Preco;
+        public int Quantidade;
+
+        
+        public double ValorTotalEmEstoque()
+        {
+            
+            return Preco * Quantidade;
+        }
+        public override string ToString()
+        {
+            return Nome + ", R$ : " + Preco.ToString("F2",CultureInfo.InvariantCulture) +", "+ Quantidade + " unidades , Total : R$ "+ValorTotalEmEstoque().ToString("F2", CultureInfo.InvariantCulture);
+        }
+
+        public void AdicionarProdutos(int novaQuantidade)
+        {
+            Quantidade = Quantidade + novaQuantidade;
+        }
+
+        public void RemoverProdutos(int novaQuantidade)
+        {
+            Quantidade = Quantidade - novaQuantidade;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using Aula42;
+using System.Globalization;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        
+        int novaQuantidade;         // cria uma variavel para repassar os valores do item que vão ser adicionados ou removidos
+        Produto p = new Produto();  // cria o objeto da classe produto
+        Console.WriteLine("Entra com os dados do produto : "); // pede para o usuario entrar com os dados
+        Console.Write("Nome : "); // pede para o usuario entrar com o nome do produto a ser cadastrado
+        p.Nome = Console.ReadLine(); // armazena o nome da variavel nome do objeto p
+        Console.Write("Preço : ");// pede para o usuario digitar o preço do produto
+        p.Preco = Double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture); // armazena o preço do produto na variavel Preco e formata o valor
+        Console.Write("Quantidade em estoque : ");// pede para o usuario entrar com a quantidade de produtos em estoque
+        p.Quantidade = int.Parse(Console.ReadLine()); //armazena a quantidade de produtos em estoque na variavel Quantidade do objeto p
+        
+
+        Console.WriteLine("Dados do produto : "+p); // mostra os dados do objeto p ja formatado
+        Console.WriteLine();
+        Console.Write("Digite o numero de produtos a serem adicionados  : ");//pede para o usuario digitar a quantidade de produtos que serão adicionados no estoque
+        novaQuantidade = int.Parse(Console.ReadLine()); //le a quantidade e armazena na variavel novaQuantidade
+        p.AdicionarProdutos(novaQuantidade); //chama o metodo AdicionarProdutos e passa como referencia a variavel novaQuantidade
+        Console.WriteLine( p);// mostra a quantidade de produtos e total ja atualizados e formatados
+        Console.WriteLine();
+        Console.Write("Digite o numero de produtos a serem removidos  : ");//pede para o usuario digitar a quantidade de produtos a serem removidos
+        novaQuantidade = int.Parse(Console.ReadLine());//le a quantidade de produtos que vão ser removidos e armazena na variavel novaQuantidade
+        p.RemoverProdutos(novaQuantidade);// chama o metodo RemoverProdutos e passa como parametro a variavel novaQuantidade
+        Console.WriteLine(p);//mostra na tela a quantidade de produtos e total ja atualizados
+    }
+}
